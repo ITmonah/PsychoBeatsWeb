ce594241426c33b0e0cd965d06003a8b47ebc472
ITmonah <vasin.sasha26@gmail.com>
2023-12-24 21:51:44 +0500

    add dwnl link

---
+++ download.js
@@ -0,0 +1,7 @@
+$('.main__download-btn').on('click', function () {
+    var link = document.createElement('a');
+    link.setAttribute('href', 'https://disk.yandex.ru/d/uukcdZA0mb3Tsg');
+    link.setAttribute('target', '_blank');
+    link.click();
+    return false;
+});
